2ba4ea694f21d33d34f838359338dc65e5e14eae
kloss <christoph.kloss@dcs-computing.com>
2015-02-18 14:41:47 +0100

    release version 3.1.0 on 2015-02-18_14-41-46

--- src/PASCAL/fix_pascal_couple.h
+++ src/PASCAL/fix_pascal_couple.h
@@ -1,15 +1,19 @@
 /* ----------------------------------------------------------------------
-   LIGGGHTS - LAMMPS Improved for General Granular and Granular Heat
+   LIGGGHTS® - LAMMPS Improved for General Granular and Granular Heat
    Transfer Simulations
 
-   LIGGGHTS is part of the CFDEMproject
+   LIGGGHTS® is part of CFDEM®project
    www.liggghts.com | www.cfdem.com
 
 
    Copyright (C): 2014 DCS Computing GmbH (www.dcs-computing.com), Linz, Austria
                   2014 Graz University of Technology (ippt.tugraz.at), Graz, Austria
 
-   LIGGGHTS is based on LAMMPS
+   LIGGGHTS® and CFDEM® are registered trade marks of DCS Computing GmbH,
+   the producer of the LIGGGHTS® software and the CFDEM®coupling software
+   See http://www.cfdem.com/terms-trademark-policy for details.
+
+   LIGGGHTS® is based on LAMMPS
    LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
    http://lammps.sandia.gov, Sandia National Laboratories
    Steve Plimpton, sjplimp@sandia.gov
@@ -22,7 +26,7 @@
 
 #ifdef FIX_CLASS
 
-FixStyle(couple/pascal,FixPaScalCouple)
+FixStyle(couple/pascal,FixParScaleCouple)
 
 #else
 
@@ -30,14 +34,14 @@ FixStyle(couple/pascal,FixPaScalCouple)
 #define LMP_FIX_PASCAL_COUPLE_H
 
 #include "fix.h"
-namespace PASCAL_NS { class PaScal; }
+namespace PASCAL_NS { class ParScale; }
 
 namespace LAMMPS_NS {
 
-class FixPaScalCouple : public Fix  {
+class FixParScaleCouple : public Fix  {
  public:
-  FixPaScalCouple(class LAMMPS *, int narg, char **arg);
-  ~FixPaScalCouple();
+  FixParScaleCouple(class LAMMPS *, int narg, char **arg);
+  ~FixParScaleCouple();
   virtual void post_create();
   void      updatePtrs();
 
@@ -45,7 +49,7 @@ class FixPaScalCouple : public Fix  {
   void      init();
   void      setup(int);
 
-  void      pre_exchange();
+//  void      pre_exchange();
   void      end_of_step();
 
   int*      get_liggghts_map(int &length);
@@ -54,25 +58,23 @@ class FixPaScalCouple : public Fix  {
 
   void*     find_push_property(const char *name, const char *type, int &len1, int &len2);
 
- protected:
-
-  class FixPropertyAtom* fix_shellTemperature_;
-  class FixPropertyAtom* fix_shellHeatFlux_;
-
  private:
 
   // data transfer is handled by this class
   class CfdDatacouplingSimple *dc_;
+  int   *map_copy;
 
   bool      verbose_;
-  int       couple_at_least_every_;
+  int       reneighbor_at_least_every_;
+  int       couple_every_,ts_create_;
   bool      couple_this_step_;
   bool      pascal_setup_;
+  bool      prePostRun_;        //indicator for printing pre and post LIGGGHTS data when running
   double    time_;
-  int       iarg_;    
+  int       iarg_;
 
-  // PaScal Object
-  PASCAL_NS::PaScal *pasc_;
+  // ParScale Object
+  PASCAL_NS::ParScale *pasc_;
 };
 
 } //end namespace
--- src/version_liggghts.h
+++ src/version_liggghts.h
@@ -1,1 +1,1 @@
-#define LIGGGHTS_VERSION "LIGGGHTS-PUBLIC 3.0.5, compiled 2014-10-31-13:52:59 by kloss"
+#define LIGGGHTS_VERSION "LIGGGHTS-WSU 3.1.0, compiled 2015-02-18-14:38:44 by kloss, git commit 63e97a993844a8ccacab49b7a1ef71181c6bdc89"
