021e16291adbda76c81117d727a0969544934b7e
kloss <christoph.kloss@dcs-computing.com>
2015-07-21 20:38:02 +0200

    release version 3.2.1 on 2015-07-21_20-38-02

--- src/version_liggghts.h
+++ src/version_liggghts.h
@@ -1,1 +1,1 @@
-#define LIGGGHTS_VERSION "LIGGGHTS-WSU 3.2.0, compiled 2015-04-28-22:00:00 by kloss, git commit 42b2db9ee9cb1e288c15448537ff5956a00940ca"
+#define LIGGGHTS_VERSION "LIGGGHTS-WSU 3.2.1, compiled 2015-07-21-20:34:46 by kloss, git commit 3d2ff8e3ac664a6998b14b39073b27068007524f"
